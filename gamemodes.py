5e8639d2f56c94ef7f341aab1d869f851b8ea8bf
Sabbir Mollah <mollahsabbir@gmail.com>
2022-09-22 02:00:07 +0600

    Added SelfDrivingGameMode, provided best model file

--- gamemodes.py
+++ gamemodes.py
@@ -149,4 +149,34 @@ class TrainGameMode:
 ============================================================
 '''       
 class SelfDrivingGameMode:
-    pass
+    def play(self):
+        track = pygame.image.load(configs.TRACK_IMAGE)
+        car_image = pygame.image.load(configs.CAR_IMAGE)
+             
+        with open(configs.MODEL_LOCATION, 'rb') as handle:
+            best_network = pickle.load(handle)
+            
+        nn_controller = NNController(best_network)
+        car = SensoredCar(car_image, nn_controller, track)
+        
+        win_width, win_height = track.get_width(), track.get_height()
+        win = pygame.display.set_mode((win_width, win_height))
+        pygame.display.set_caption("Self Driving Car")
+        
+        event_listeners = [GameExitEventListener()]
+        for listener in event_listeners:
+            EventHandler.register(listener)
+            
+        run = True
+        clock = pygame.time.Clock()
+        
+        while run:
+            clock.tick(configs.FPS)
+            win.blit(track, (0,0))
+            car.update(win)
+            pygame.display.update()
+            
+            for event in pygame.event.get():
+                EventHandler.notify(event)
+                    
+        pygame.quit()
